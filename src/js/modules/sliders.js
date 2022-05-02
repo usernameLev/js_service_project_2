7d8879f5b5fc1cbdc1da7bebb72532deadcd6852
Lev <tyanlev1989@gmail.com>
2022-05-02 19:25:31 +0500

    testik

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,30 +1,68 @@
-const sliders = (slides, dir, prev, next) => {
-  let slideIndex = 1;
-  const items = document.querySelectorAll(slides);
-    
+export const sliders = ({ slides, dir, prev, next }) => {
+  let slideIndex = 1,
+    paused = false;
+  const listSlides = document.querySelectorAll(slides);
 
   const showSlides = (n) => {
-    if (n > items.length) {
+    if (n > listSlides.length) {
       slideIndex = 1;
     }
 
     if (n < 1) {
-      slideIndex = items.length;
+      slideIndex = listSlides.length;
     }
 
-    items.forEach((item) => {
-      item.classList.add('animated');
-      item.style.display = 'none';
+    listSlides.forEach((listSlide) => {
+      listSlide.classList.add('animated');
+      listSlide.style.display = 'none';
     });
 
-    items[slideIndex - 1].style.display = 'block';
+    listSlides[slideIndex - 1].style.display = 'block';
   };
 
   showSlides(slideIndex);
 
-  const plussSlides = (n) => {
-    showSlides(slideIndex += n);
-  }
-};
+  const plusSlides = (n) => {
+    showSlides((slideIndex += n));
+  };
 
-export default sliders;
+  try {
+    const prevBtn = document.querySelector(prev),
+      nextBtn = document.querySelector(next);
+
+    prevBtn.addEventListener('click', () => {
+      plusSlides(-1);
+      listSlides[slideIndex - 1].classList.remove('slideInLeft');
+      listSlides[slideIndex - 1].classList.add('slideInRight');
+    });
+
+    nextBtn.addEventListener('click', () => {
+      plusSlides(1);
+      listSlides[slideIndex - 1].classList.remove('slideInRight');
+      listSlides[slideIndex - 1].classList.add('slideInLeft');
+    });
+  } catch (e) {}
+
+  const activateAnimation = () => {
+    if (dir === 'vertical') {
+      paused = setInterval(function () {
+        plusSlides(1);
+        listSlides[slideIndex - 1].classList.add('slideInDown');
+      }, 3000);
+    } else {
+      paused = setInterval(function () {
+        plusSlides(1);
+        listSlides[slideIndex - 1].classList.remove('slideInRight');
+        listSlides[slideIndex - 1].classList.add('slideInLeft');
+      }, 3000);
+    }
+  };
+  activateAnimation();
+
+  listSlides[0].parentNode.addEventListener('mouseenter', () => {
+    clearInterval(paused);
+  });
+  listSlides[0].parentNode.addEventListener('mouseleave', () => {
+    activateAnimation();
+  });
+};
