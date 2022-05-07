d4cf09197f8d952ce9e18670177e5e8c79a47c25
Lev <tyanlev1989@gmail.com>
2022-05-07 20:41:21 +0500

    024 lesson 1 commit

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,7 @@ import {
   filter,
   pictureSize,
   accordion,
+  drop,
 } from './modules';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -61,4 +62,6 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   pictureSize('.sizes-block');
+
+  drop();
 });
